e2069262db5920d4e604e8b67f8c9fba428eb820
Daniel Llano <dllanob0806@gmail.com>
2022-08-22 15:08:00 -0500

    Change config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'server',
-
+  ssr: true,
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
@@ -60,6 +60,10 @@ export default {
   /*
   ** Server Middleware
   */
+  serverMiddleware: [
+    // API middleware
+    '~/api/index.js'
+  ]
 
   /*
   ** For deployment you might want to edit host and port
@@ -73,6 +77,4 @@ export default {
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
   */
-  build: {
-  }
 }
